a279c94fb8600a0c10b58c303ad76b420decbaee
PunitPunia <punitpunia54@gmail.com>
2026-05-22 17:56:14 +0530

    Refactor main.py to include CORS middleware and add index.html and redteam.db files for attack simulations

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 from app.api.routes import router
 
-from app.core.database import engine
+from app.core.database import Base, engine
 from app.models.attack import AttackSimulation
+
+from fastapi.middleware.cors import CORSMiddleware
+    
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
